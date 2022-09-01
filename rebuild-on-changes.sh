794881157a2cb1888a8c17273c0783a8e4e920bb
RayChimera <chimera@chimera.website>
2022-09-01 13:22:40 +0400

    Small fix to watch all dirs, not only /

--- rebuild-on-changes.sh
+++ rebuild-on-changes.sh
@@ -1,6 +1,6 @@
 export DOCKER_BUILDKIT=1
 
-while inotifywait -q -e create -e modify -e delete -e move app-in-development
+while inotifywait -q -e create -e modify -e delete -e move app-in-development app-in-development/pages app-in-development/pages/api app-in-development/public app-in-development/styles
   do
     docker build . -t nextjs --target dev
     docker rm -f nextjs
